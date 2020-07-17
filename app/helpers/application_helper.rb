f825930dafb4928a760895e7d263cabe68419eec
ericmbouwe <ericmbouwe@yahoo.fr>
2020-07-17 13:45:58 +0100

    fixed rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,23 @@
 module ApplicationHelper
-
-  def set_user_avatar(obj)
-    if obj.avatar.attached? 
-      image_tag obj.avatar, class: "mt-5 rounded-circle avatar", width: 90, height: 90 
-    else 
-      image_tag 'sample-profile2.png', alt: "default image", width: 90, height: 90, class: "rounded-circle mt-5" 
-    end 
+  def add_user_avatar(obj)
+    if obj.avatar.attached?
+      image_tag obj.avatar, class: 'mt-5 rounded-circle avatar', width: 90, height: 90
+    else
+      image_tag 'sample-profile2.png', alt: 'default image', width: 90, height: 90, class: 'rounded-circle mt-5'
+    end
   end
 
-  def set_program_item(obj)
-    if obj
-      set_group_icon(obj)
-    end
+  def add_program_item(obj)
+    add_group_icon(obj) if obj
   end
 
   private
 
-  def set_group_icon(obj)
+  def add_group_icon(obj)
     if obj.avatar.attached?
-      image_tag obj.avatar, class: "avatar", width: 70, height: 70
+      image_tag obj.avatar, class: 'avatar', width: 70, height: 70
     else
-      image_tag 'sample-group.jpg', alt: "default image", width: 70, height: 70, class: ""
+      image_tag 'sample-group.jpg', alt: 'default image', width: 70, height: 70
     end
   end
 end
