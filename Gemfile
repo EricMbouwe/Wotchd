2bf09f9c931d7795ffde32277fbe468fb0fe9286
ericmbouwe <ericmbouwe@yahoo.fr>
2020-07-18 14:10:13 +0100

    more tests added for features

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.7'
 
 # Reduces boot times through caching;https://github.com/karthi07/restaurant_page_js/issues/2 required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'coderay'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -23,4 +23,63 @@ feature 'features', type: :feature do
       expect(page).to have_content 'Dann'
     end
   end
+
+  feature 'create a new program' do
+    before(:each) do
+      User.create(name: 'damien', email: 'dal@server.com', password: 'abcdef')
+      visit new_session_path
+      fill_in 'name', with: 'damien'
+      click_on 'Login'
+    end
+
+    scenario 'with invalid params' do
+      visit new_program_path
+      fill_in 'program_name', with: 'dbz'
+      click_on 'Add a program'
+      expect(page).to have_content "Amount can't be blank"
+    end
+
+    scenario 'with valid params' do
+      visit new_program_path
+      fill_in 'program_name', with: 'good doctor'
+      fill_in 'program_amount', with: 54
+      click_on 'Add a program'
+      expect(page).to have_content 'Good doctor'
+    end
+  end
+
+  feature 'create a new group' do
+    before(:each) do
+      User.create(name: 'damien', email: 'dal@server.com', password: 'abcdef')
+      visit new_session_path
+      fill_in 'name', with: 'damien'
+      click_on 'Login'
+    end
+
+    scenario 'with invalid params' do
+      visit new_group_path
+      click_on 'Create Group'
+      expect(page).to have_content "Name can't be blank"
+    end
+  end
+
+  feature 'renders the list of TV shows' do
+    before(:each) do
+      User.create(name: 'damien', email: 'dal@server.com', password: 'abcdef')
+      visit new_session_path
+      fill_in 'name', with: 'damien'
+      click_on 'Login'
+    end
+
+    scenario 'All TV shows' do
+      visit user_path(User.find_by(name: 'damien').id)
+      click_on 'All my TV Shows'
+      expect(page).to have_content 'My TV Shows'
+    end
+
+    scenario 'All shuffled TV shows' do
+      visit programs_path('ungrouped')
+      expect(page).to have_content 'My Shuffled TV Shows'
+    end
+  end
 end
