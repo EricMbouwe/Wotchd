675537f1496bb281433d30c7e816310461359ae3
ericmbouwe <ericmbouwe@yahoo.fr>
2020-07-18 09:41:43 +0100

    added domain name to production env

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'http://localhost:3000' # production server of the front-end app
+    origins 'https://wotchd.herokuapp.com/' # production server of the front-end app
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :option, :head],
                   credentials: true # allow to pass the cookies from the front-end app to the back-end
   end
