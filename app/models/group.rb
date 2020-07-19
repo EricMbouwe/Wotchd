c9c5ceb53121ab2b3f4f74539303837188e572a1
ericmbouwe <ericmbouwe@yahoo.fr>
2020-07-19 22:09:47 +0100

    added validations for images upload

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,15 @@ class Group < ApplicationRecord
   has_one_attached :avatar
 
   validates :name, presence: true, uniqueness: true, length: { in: 3..10 }
+  validate :acceptable_avatar
 
   scope :order_by_name, -> { order('name DESC') }
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+
+  def acceptable_avatar
+    return errors.add(:avatar, 'is too big') unless avatar.byte_size <= 1.megabyte
+
+    acceptable_types = ['image/jpeg', 'image/png']
+    return errors.add(:avatar, 'must be a JPEG or PNG') unless acceptable_types.include?(avatar.content_type)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,16 @@ class User < ApplicationRecord
   validates_presence_of :email
   validates_presence_of :password
   validates_uniqueness_of :email
+  validate :acceptable_avatar
 
   has_one_attached :avatar
   has_many :programs, inverse_of: 'author', dependent: :destroy
   has_many :programs, -> { includes :group }, class_name: 'Program'
+
+  def acceptable_avatar
+    return errors.add(:avatar, 'is too big') unless avatar.byte_size <= 1.megabyte
+
+    acceptable_types = ['image/jpeg', 'image/png']
+    return errors.add(:avatar, 'must be a JPEG or PNG') unless acceptable_types.include?(avatar.content_type)
+  end
 end
